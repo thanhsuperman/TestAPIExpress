df25af4011b0f1ddcc36ac90d8370538ca1766a7
Ngan Nguyen Minh <ngan.nguyen3@citek.vn>
2025-03-25 07:03:27 +0000

    CO RS

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3131;
+
+app.use(cors());
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Xin chào từ API Express.js!' });
